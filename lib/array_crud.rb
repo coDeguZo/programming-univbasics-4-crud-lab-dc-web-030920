6306a9c47ed13efb5dd2151ea984cda124de8a22
Nduka-Eze Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-04 18:38:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -35,12 +35,12 @@ end
 
 def retrieve_first_element_from_array(array)
   array = []
-  array = 0
+  return array[0]
 end
 
 def retrieve_last_element_from_array(array)
   array = []
-  array[-1]
+  return array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
