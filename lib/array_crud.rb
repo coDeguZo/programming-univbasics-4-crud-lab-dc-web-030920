1261670fc72224ab5b63352063a4e4e88f27b17a
Nduka-Eze Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-04 18:27:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,7 +10,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["wow", "I", "am", "really", "learning"]
-  element.push = "array!"
+  element.push("array!")
   return array
 end
 
