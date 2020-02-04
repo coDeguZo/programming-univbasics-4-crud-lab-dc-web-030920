2e9c9095b327b889698b7fc85fc74e876822e6e4
Nduka-Eze Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-04 18:43:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,8 +25,9 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array = []
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   add_beginning = array.shift
+  add_beginning
 end
 
 def retrieve_element_from_index(array, index_number)
