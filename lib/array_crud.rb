382ac60147c2b93598697742b3a1784649871aa8
Nduka-Eze Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-04 18:45:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,8 @@ end
 
 def retrieve_element_from_index(array, index_number)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  return array[index_number]
+  index_number = 2
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
